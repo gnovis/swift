755a4aa3fb7a4fbba8681433a3166c77688cbf8b
Jan Nováček <novacekj5@gmail.com>
2015-03-14 15:41:14 +0100

    added class hierarchy of scaled attributes

--- swift.py
+++ swift.py
@@ -1,32 +1,86 @@
-#!/bin/python
+#!/bin/python3
 """Main file of swift - FCA data converter"""
 
-import re
 
-class Attribute(object):
-    def __init__(self, index, name=""):
-        self.__name = name
-        self.__index = index
+class Attribute:
+    def __init__(self, index, name, attr_type):
+        self._name = name
+        self._index = index
+        self._attr_type = attr_type
 
     @property
     def name(self):
-        return self.__name
-
-    @name.setter
-    def name(self, value):
-        self.__name = value
+        return self._name
 
     @property
     def index(self):
-        return self.__index
+        return self._index
+
+    @property
+    def attr_type(self):
+        return self._attr_type
+
+    class AttrType:
+        NUMERIC = 0
+        NOMINAL = 1
+        STRING = 2
 
 
-A = Attribute(0, "vaha")
-A.name = "delka"
-# print A.name, A.index
+class AttrScale(Attribute):
+    def __init__(self, index, name, attr_type,
+                 attr_pattern=None, expr_pattern=None):
+        super().__init__(index, name, attr_type)
+
+        # name of atrribute, it is pattern to scaling
+        self._attr_pattern = attr_pattern
+        # expression to scaling
+        self._expr_pattern = expr_pattern
+
+    def scale(self, attrs, values):
+        """
+        Scale value according pattern.
+        attrs - dict with names(keys) and indexes(values)
+        values - values of current procces row
+        """
+        val_index = attrs[self._attr_pattern]
+        return values[val_index]
+
+
+class AttrScaleNumeric(AttrScale):
+    def __init__(self, index, name, attr_pattern, expr_pattern):
+        super().__init__(index, name, self.AttrType.NUMERIC,
+                         attr_pattern, expr_pattern)
+
+    def scale(self, attrs, values):
+        x = super().scale(attrs, values)  # NOQA
+        return int(eval(self._expr_pattern))  # int cast, because want 0 or 1
+
+
+class AttrScaleEnum(AttrScale):
+    def __init__(self, index, name, attr_pattern, expr_pattern):
+        super().__init__(index, name, self.AttrType.NOMINAL,
+                         attr_pattern, expr_pattern)
+
+
+class AttrScaleString(AttrScale):
+    def __init__(self, index, name, attr_pattern, expr_pattern):
+        super().__init__(index, name, self.AttrType.STRING,
+                         attr_pattern, expr_pattern)
+
+
+attrs_old = {val: i for i, val in enumerate(['age', 'length', 'height'])}
+values = [50, 745, 152]
+
+attr = AttrScaleNumeric(0, 'scale-age', 'age', '(x<70)')
+print(attr.scale(attrs_old, values))
+
+"""
+import re
+#create dict from list
+#{val : i for i, val in enumerate(l)}
 
 pattern = re.compile("(\w+)=(\w+)" +
-                     "((?:\((?:[0-9]+(?:>=|<=|>|<))?x(?:>=|<=|>|<)[0-9]+\)i)|" +
+                     "((?:\((?:[0-9]+(?:>=|<=|>|<))?x(?:>=|<=|>|<)[0-9]+\)i)|" +  # NOQA
                      "(?:\(.*\)r))?")
 
 attrs = pattern.findall("a=b, novy1=puvodni1(10<=x<=100)i, novy2=puvodni2(x<=50)i, novy3=puvodni3(man)r")
@@ -37,4 +91,4 @@ for at in attrs:
 m = "(a[ho?j]ky)r"
 new = m[1:-2]
 print new
-
+"""
