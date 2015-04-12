9db0e66a8d171ff405f61fdb0b475df4676ce8d6
Jan Nováček <novacekj5@gmail.com>
2015-04-12 14:16:09 +0200

    Parsing arguments done from cmd line done

--- source/swift.py
+++ source/swift.py
@@ -1,7 +1,46 @@
 #!/bin/python3
 """Main file of swift - FCA data converter"""
 
-from convertor_fca import Convertor
+import argparse
+# from convertor_fca import Convertor
+
+SOURCE_ARGS = {"source": "source",
+               "source_separator": "separator",
+               "source_attributes": "str_attrs"}
+TARGET_ARGS = {"target": "source",
+               "target_separator": "separator",
+               "target_attributes": "str_attrs",
+               "target_objects": "str_objects",
+               "relation_name": "relation_name"}
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-s", "--source", help="Name of source file")
+parser.add_argument("-ss", "--source_separator",
+                    help="Separator whitch is used in source file. Default is ','")
+parser.add_argument("-sa", "--source_attributes", help="Source file (old) attributes")
+parser.add_argument("-t", "--target", help="Name of target file")
+parser.add_argument("-ts", "--target_separator",
+                    help="Separator whitch will be used in target file. Default is ','")
+parser.add_argument("-ta", "--target_attributes", help="Target file (new) attributes")
+parser.add_argument("-to", "--target_objects", help="Target file (new) objects")
+parser.add_argument("-rn", "--relation_name", help="New name of relation")
+
+args = parser.parse_args()
+
+old_file_args = {}
+new_file_args = {}
+
+for key, val in vars(args).items():
+    if key in SOURCE_ARGS:
+        new_key = SOURCE_ARGS[key]
+        old_file_args[new_key] = val
+    elif key in TARGET_ARGS:
+        new_key = TARGET_ARGS[key]
+        new_file_args[new_key] = val
+
+print(old_file_args)
+print(new_file_args)
 
 # Notes
 #
@@ -35,8 +74,8 @@ new_str_objects_2 = "0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13"
 old = dict(source="test.dat")
 new = dict(source="dat.arff")
 
-convertor = Convertor(old, new)
-convertor.convert()
+# convertor = Convertor(old, new)
+# convertor.convert()
 
 
 """
