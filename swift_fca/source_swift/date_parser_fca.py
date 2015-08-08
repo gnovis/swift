f9966f231d8c1a8e6fe00230f942bbb969664565
Jan Nováček <novacekj5@gmail.com>
2015-08-08 15:30:01 +0200

    Parsing parameters done (prepared), TODO include to swift

--- swift_fca/source_swift/date_parser_fca.py
+++ swift_fca/source_swift/date_parser_fca.py
@@ -1,5 +1,23 @@
 from datetime import datetime
 
+
+class DateParser():
+
+    ISO_FORMAT = "%Y-%m-%dT%H:%M:%S"
+
+    def __init__(self, pattern):
+        self._pattern = pattern
+
+    def get_time_stamp(self, date):
+        date = datetime.strptime(date, self._pattern)
+        return date.timestamp()
+
+    def get_format(self):
+        return self._pattern
+
+    def time_stamp_to_str(self, ts):
+        return datetime.fromtimestamp(ts).strftime(self._pattern)
+
 """
 
 With this code is possible to parse java SimpleDateFormat
@@ -25,21 +43,3 @@ class JavaDateParser():
     def time_stamp_to_str(self, ts):
         return ts
 """
-
-
-class DateParser():
-
-    ISO_FORMAT = "%Y-%m-%dT%H:%M:%S"
-
-    def __init__(self, pattern):
-        self._pattern = pattern
-
-    def get_time_stamp(self, date):
-        date = datetime.strptime(date, self._pattern)
-        return date.timestamp()
-
-    def get_format(self):
-        return self._pattern
-
-    def time_stamp_to_str(self, ts):
-        return datetime.fromtimestamp(ts).strftime(self._pattern)
